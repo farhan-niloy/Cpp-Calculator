dd52f40ea810894124e89cd7cc060dfbb235f0b5
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-08-03 16:48:14 +0600

    add my calculator

--- My Calculator Using Cpp/My Calculator Using Cpp/main.cpp
+++ My Calculator Using Cpp/My Calculator Using Cpp/main.cpp
@@ -13,35 +13,35 @@ using namespace std;
 double calculator_function (double x, double y, char c);
 
 int main(int argc, const char * argv[]) {
-    double void1{}, void2{}, voidOp{};
+    double void1{}, void2{};
     
-    cout<<"Welcome To Farhan's Calculator Dedicated To Omi Farhan, Rajuk  Hsc Batch 2024, (a frog emoji)" <<endl;
+    char voidOp{};
+    
+    cout<<"Welcome To Farhan's Calculator Dedicated To Omi Farhan, Rajuk College Hsc Batch 2024, (a frog emoji)" <<endl;
     cout<<"\nEnter First Number: " ;
     cin>>void1;
     cout<<"Enter Second Number: " ;
     cin>>void2;
-     
-    cout<<"Enter Operation ( + - * / ^ Q = quit (❤): " ;
+    
+    cout<<"Enter Operation ( + - * / ^ Q = quit (O): " ;
     cin>>voidOp;
     
-    cout<<"Result: " <<calculator_function(void1, void2, voidOp);
-    
-    while (voidOp!= 'Q' ||'q') {
+    while (voidOp != 'Q'||'q') {
+        
+        cout<<"Result: " <<calculator_function(void1, void2, voidOp) <<endl <<endl;
+        
         cout<<"Welcome To Farhan's Calculator Dedicated To Omi Farhan, Rajuk  Hsc Batch 2024, (a frog emoji)" <<endl;
         cout<<"\nEnter First Number: " ;
         cin>>void1;
         cout<<"Enter Second Number: " ;
         cin>>void2;
-         
-        cout<<"Enter Operation ( + - * / ^ Q = quit (❤): " ;
+        
+        cout<<"Enter Operation ( + - * / ^ Q = quit (O): " ;
         cin>>voidOp;
-        
-        cout<<"Result: " <<calculator_function(void1, void2, voidOp);
-        
-        
     };
-
-};
+    
+    return 0;
+}
 
 double calculator_function (double x, double y, char c){
     
@@ -57,8 +57,8 @@ double calculator_function (double x, double y, char c){
         cout<<x/y;
     } else if (c=='^'){
         cout<<pow(x, y);
-    } else if (c=='O') {
-        cout<<"I love Antara Mubassira Omi so much. I don't know why, She is so facinating to me. I love everything about her. I feel her. It's transcendent. Email me if you notice: farhansarkerniloy@gmail.com";
+    }else {
+        cout<<"invalid input";
     }
     
     return result;
